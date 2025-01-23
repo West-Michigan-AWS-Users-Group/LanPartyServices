3c81cbc743c8d6964cfa7bc67d603d1e906e99ad
tnielsen2 <trentnielsen84@gmail.com>
2025-01-23 08:37:48 -0500

    Update ini to working version

--- lan_party_services/ut2k4/entrypoint.sh
+++ lan_party_services/ut2k4/entrypoint.sh
@@ -2,9 +2,10 @@
 
 
 cp -r /usr/src/System/* /usr/src/ut2004/System
-cp /usr/src/ut2004/System/UT2004.ini /usr/src/ut2004/System/UT2004.ini.tpl
 chmod 777 /usr/src/ut2004/System/*
-
+cp /ini/UT2004.ini /usr/src/ut2004/System/UT2004.ini
+cp /usr/src/ut2004/System/UT2004.ini /usr/src/ut2004/System/UT2004.ini.tpl
+cat /usr/src/ut2004/System/UT2004.ini.tpl
 set -e
 
 if [ "$1" = 'ucc-bin' ]; then
