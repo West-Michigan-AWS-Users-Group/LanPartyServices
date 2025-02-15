240f7ebdca48942604b8288ec8b0cbdef13475a3
tnielsen2 <trentnielsen84@gmail.com>
2025-02-15 17:26:15 -0500

    Update stack name to have proper production prefix

--- lan_party_services/discord_bot/app/aws.py
+++ lan_party_services/discord_bot/app/aws.py
@@ -14,7 +14,7 @@ def check_stacks_exist(stack_names: list) -> bool:
 
     for stack_name in stack_names:
         try:
-            response = cf_client.describe_stacks(StackName=stack_name)
+            response = cf_client.describe_stacks(StackName=f'prod-lan-party-services-{stack_name}')
             if not response["Stacks"]:
                 return False
         except cf_client.exceptions.ClientError:
